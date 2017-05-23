2b4bb1199d090edd73dac5798b4397391e792bd2
RobertBWeidlich <BobWeidlich@gmail.com>
2017-05-23 16:34:00 -0400

    Continued work on properties.

--- src/main/java/com/cn/piaware/storm/topology/PiAwareTopology.java
+++ src/main/java/com/cn/piaware/storm/topology/PiAwareTopology.java
@@ -1,6 +1,11 @@
 package com.cn.piaware.storm.topology;
 
+import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.Properties;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.commons.configuration2.CompositeConfiguration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
@@ -8,11 +13,32 @@ import org.apache.commons.configuration2.SystemConfiguration;
 
 public class PiAwareTopology {
     public static void main(String[] args) {
+
+        /*
+         * 1. print properties in alphbetical order
+         */
+        Properties props = System.getProperties();
+        Enumeration<Object> ek = props.keys();
+        Collections.sort(ek);
+
+        while (ek.hasMoreElements()) {
+            String key = ek.nextElement().toString();
+            String val = props.get(key).toString();
+            System.out.println(key + "=" + val);
+        }
+
+        List<Object> ll = new ArrayList(ek);
+
+
+        /*
+         * 2. read properties file from correct place in Maven File Directory Structure
+         */
         CompositeConfiguration config = new CompositeConfiguration();
 
         //config.addConfiguration(new PropertiesConfiguration("piaware.properties"));
-        config.addConfiguration(new PropertiesConfiguration(""));
+        //config.addConfiguration(new PropertiesConfiguration(""));
 
+        /*
         Iterator<String> keys = config.getKeys();
 
         String kk = keys.toString();
@@ -22,6 +48,7 @@ public class PiAwareTopology {
             String val = config.getProperty(key).toString();
             System.out.println(key + " = " + val);
         }
+        */
 
     }
 }
