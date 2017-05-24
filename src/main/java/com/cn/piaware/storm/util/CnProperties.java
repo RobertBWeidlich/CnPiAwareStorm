5fd7ffe6addee0b59b87fe79467afb0bf8f45fe0
rbw <rbw@acme.com>
2017-05-24 18:33:53 -0400

    Moved properties code, which is still in progress, to new file.

---
+++ src/main/java/com/cn/piaware/storm/util/CnProperties.java
@@ -0,0 +1,67 @@
+package com.cn.piaware.storm.util;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.commons.configuration2.CompositeConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.SystemConfiguration;
+
+
+public class CnProperties {
+    static void properties() {
+
+        int[] ii = {1, 2, 3};
+        for (int i : ii) {
+            System.out.println(i);
+        }
+
+
+        /*
+         * 1. print properties in alphbetical order
+         */
+        Properties props = System.getProperties();
+        Enumeration<String> ek = props.keys();
+        //Collections.sort(ek);
+
+        while (ek.hasMoreElements()) {
+            //String key = ek.nextElement().toString();
+            String key = ek.nextElement();
+            String val = props.get(key).toString();
+            System.out.println(key + "=" + val);
+        }
+
+        //List<Object> ll = new ArrayList(ek);
+
+
+        /*
+         * 2. read properties file from correct place in Maven File Directory Structure
+         */
+        //CompositeConfiguration config = new CompositeConfiguration();
+
+        //config.addConfiguration(new PropertiesConfiguration("piaware.properties"));
+        //config.addConfiguration(new PropertiesConfiguration(""));
+
+        /*
+        Iterator<String> keys = config.getKeys();
+
+        String kk = keys.toString();
+
+        while (keys.hasNext()) {
+            String key = keys.next();
+            String val = config.getProperty(key).toString();
+            System.out.println(key + " = " + val);
+        }
+        */
+
+
+
+
+
+
+    }
+}
